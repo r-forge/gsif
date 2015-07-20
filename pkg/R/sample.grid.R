d5434614008a01bfc736986d44b9827a3fe14d41
thengl (Dr ) <tom.hengl@opengeohub.org>
2015-07-20 14:48:31 +0000

    added sample.grid.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gsif@217 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: a01cf5550c8ca84da77cb54fb26b201cd0f67230

--- pkg/R/sample.grid.R
+++ pkg/R/sample.grid.R
@@ -3,53 +3,52 @@
 # Contributions  : Tomislav Hengl (tom.hengl@wur.nl);
 # Status         : tested
 # Note           :
-#
 
 
 sample.grid.SpatialPointsDataFrame <- function(obj, cell.size, n, bbox, ...){
     #to avoid the gid in obj
     if ("gid" %in% names(obj)){
-        gid <- obj$gid
+      gid <- obj$gid
     }else{
-        gid <- NULL
+      gid <- NULL
     }
     obj$gid <- 1:length(obj)
     if(missing(bbox)) { bbox <- obj@bbox }
     if(missing(cell.size)){
-        # print warning:
-        if(length(obj)>10000){
-        warning("You need to assgn cell.size", immediate. = TRUE)
-        }
+        ## automatically determine width:
+        cell.size <- bbox[1,]/400 
+        message("Assigning 'cell.size'", immediate. = TRUE)
     }
-    require(raster)
     x <- GridTopology(cellcentre.offset=bbox[,1],
             cellsize=cell.size,
-            cells.dim=c(ceiling(abs(diff(bbox[1,])/cell.size[1])),
-            ncols=ceiling(abs(diff(bbox[2,])/cell.size[2]))))
+            cells.dim=c(floor(abs(diff(bbox[1,])/cell.size[1])),
+            ncols=floor(abs(diff(bbox[2,])/cell.size[2]))))
     r.sp <- SpatialGrid(x, proj4string = obj@proj4string)
     r <- raster(r.sp)
-    in.r <- rasterize(obj, r, field = "gid")
-    grd <- as(in.r, "SpatialGridDataFrame")
+    grd <- as(rasterize(obj, r, field = "gid"), "SpatialGridDataFrame")
     ov <- over(obj, grd, ...)
     pnts <- tapply(obj$gid, ov, function(x, n){
-        if(length(x) <= n) x
-        else sample(x,n)
+        if(length(x) <= n){ x
+        } else { sample(x,n) }
         }, n=n)
     pnts <- unlist(pnts, use.names = FALSE)
-    ret <- obj[obj$gid %in% pnts, ]
+    ret <- list(obj[obj$gid %in% pnts, ], grd)
+    names(ret) <- c("subset", "grid")
     if(is.null(gid)){
-        ret$gid <- NULL
+      ret$subset$gid <- NULL
     }else{
-        ret$gid <- gid
+      ret$subset$gid <- gid
     }
     return(ret)
 }
+
 sample.grid.SpatialPoints <- function(obj, cell.size, n, bbox, ...){
     obj <- SpatialPointsDataFrame(obj, data.frame(id = 1:length(obj)))
     ret <- sample.grid.SpatialPointsDataFrame(obj, cell.size, n, bbox, ...)
     ret <- as(ret, "SpatialPoints")
     return(ret)
 }
+
 sample.grid <- function(obj, cell.size, n, bbox, ...) UseMethod("sample.grid")
 setMethod("sample.grid", "SpatialPoints", sample.grid.SpatialPoints)
 setMethod("sample.grid", "SpatialPointsDataFrame", sample.grid.SpatialPointsDataFrame)
