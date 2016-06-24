468375e35cb9955e1cde32ada36d493443d3ac25
thengl (Dr ) <tom.hengl@opengeohub.org>
2016-06-24 12:51:06 +0000

    updated autopredict
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gsif@230 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 942eb45a6e11b877239032e194d594f05de2d85f

--- pkg/R/buffer.dist.R
+++ pkg/R/buffer.dist.R
@@ -8,13 +8,18 @@
 setMethod("buffer.dist", signature(observations = "SpatialPointsDataFrame", predictionDomain = "SpatialPixelsDataFrame"), function(observations, predictionDomain, classes, width, ...){
   if(missing(width)){ width <- sqrt(areaSpatialGrid(predictionDomain)) }
   if(!length(classes)==length(observations)){ stop("Length of 'observations' and 'classes' does not match.") }
+  ## remove classes without any points:
+  xg = summary(classes, maxsum=length(levels(classes)))
+  selg.levs = attr(xg, "names")[xg > 1]
+  if(length(selg.levs)<length(levels(classes))){
+    fclasses <- as.factor(classes)
+    fclasses[which(!fclasses %in% selg.levs)] <- NA
+    classes <- droplevels(fclasses)
+  }
+  ## derive buffer distances
   s <- list(NULL)
   for(i in 1:length(levels(classes))){
-    pnts <- observations[which(classes==levels(classes)[i]),1]@coords
-    if(!is.null(pnts)&nrow(pnts)>0){
-      r <- rasterize(pnts, y=raster(predictionDomain))
-      s[[i]] <- distance(r, width=width, ...)
-    }
+    s[[i]] <- raster::distance(rasterize(observations[which(classes==levels(classes)[i]),1]@coords, y=raster(predictionDomain)), width=width, ...)
   }
   s <- s[sapply(s, function(x){!is.null(x)})]
   s <- brick(s)
